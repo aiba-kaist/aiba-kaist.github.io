30b48e65d3e822a57725c6f1732d3e833dfda262
aiba-kaist <kaist.slab@gmail.com>
2026-08-12 10:30:21 +0000

    Deploying to gh-pages from @ aiba-kaist/aiba-kaist.github.io@e0de4081d2f73caad4adb993db51276c8e96aa68 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-industry",
           title: "industry",
           description: "Our industry partners and collaborations",
